72e4552c97b64349b7e19e6bacf9591e00248760
Rădulescu Carla Gabriela <carlagabrielaradulescu@gmail.com>
2024-05-20 22:36:54 +0200

    Add some TODOs for my future plans

--- src/model/customer.py
+++ src/model/customer.py
@@ -1,14 +1,19 @@
-# TODO list
 from typing import List
 from .order import Order
 
 
 class Customer(object):
-    # TODO think of adding/removing attributes
-    def __init__(self, customer_id: int, customer_name: str, age:int, customer_gender: str, customer_address: str):
+    def __init__(self, customer_id: int, customer_name: str, customer_age: int, customer_gender: str,
+                 customer_address: str):
         self.customer_id: int = customer_id
         self.customer_name: str = customer_name
-        self.age: int = age
+        self.customer_age: int = customer_age
         self.customer_gender: str = customer_gender
         self.customer_address: str = customer_address
         self.order_history: List[Order] = []
+
+
+# TODO:
+#  statistics for age/gender
+#  offer recommendations based on the history
+#  make it have a valid age? 10 < < 100
