a763406d7669e591cb535054ba32f9434d3f625b
Rădulescu Carla Gabriela <carlagabrielaradulescu@gmail.com>
2024-05-20 22:26:35 +0200

    Add some TODOs for my future plans

--- src/model/employee.py
+++ src/model/employee.py
@@ -1,7 +1,14 @@
 class Employee(object):
-    # TODO think about adding salary, gender, age, position etc.
-    def __init__(self, employee_id: int, employee_name: str, employee_age: int):
+    def __init__(self, employee_id: int, employee_name: str, employee_age: int, salary: float, employee_gender: str):
         self.employee_id: id = employee_id
         self.employee_name: str = employee_name
         self.employee_age: int = employee_age
-        # self.salary = 0
+        self.employee_gender: str = employee_gender
+        self.salary: float = salary
+
+# TODO:
+#  to work you should have the age of 16+ <= 50
+#  salary > 0
+#  add, remove, update employees from the system
+#  maybe make a statistics based on age/gender/salary
+#  or some additional things about the salary
