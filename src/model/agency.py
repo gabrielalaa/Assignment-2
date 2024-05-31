e145c3e66a27d81a22e222b34b0fb0632ad46c28
Rădulescu Carla Gabriela <carlagabrielaradulescu@gmail.com>
2024-06-01 00:42:10 +0200

    Include methods for employees

--- src/model/agency.py
+++ src/model/agency.py
@@ -1,7 +1,7 @@
-from .database import Session, Customer_db
+from .database import Session, Customer_db, Employee_db
 
 
-# Menu_db, Order_db , Employee_db
+# Menu_db, Order_db
 
 
 # Create a class to import all the methods easily
@@ -44,7 +44,7 @@ class Agency:
             # Find the first customer with the given ID
             customer = self.session.query(Customer_db).filter(Customer_db.customer_id == customer_id).first()
             if customer is None:
-                return None
+                raise ValueError(f"A customer with ID {customer_id} does not exist!")
             return {obj.name: getattr(customer, obj.name) for obj in customer.__table__.columns}
         except Exception as e:
             raise Exception(f"An error occurred while getting the customer: {str(e)}")
@@ -82,3 +82,70 @@ class Agency:
             self.session.close()
 
     # TODO: update_customer
+    #  try to figure it out the error: when I try to add a person with a wrong gender -> then try to get them all
+
+    # METHODS for employee
+    def add_employee(self, employee_data):
+        try:
+            # Assert that ID does not exist
+            existing_employee = self.session.query(Employee_db).filter_by(
+                employee_id=employee_data['employee_id']).first()
+            if existing_employee:
+                raise ValueError(f"An employee with ID {employee_data['employee_id']} already exists!")
+
+            # Create a new_employee, extracting his/her data
+            new_employee = Employee_db(**employee_data)
+            # Add the new_employee to the database
+            self.session.add(new_employee)
+            # Commit the changes
+            self.session.comit()
+            # Convert SQLAlchemy object to a dictionary
+            return {obj.name: getattr(new_employee, obj.name) for obj in new_employee.__table__.columns}
+        except Exception as e:
+            # Ensure that none of the changes take effect
+            self.session.rollback()
+            raise ValueError(f"An error occurred while adding the employee: {str(e)}")
+        finally:
+            # Close the session
+            self.session.close()
+
+    def get_employee(self, employee_id):
+        try:
+            # Find the first employee with the given ID
+            employee = self.session.query(Employee_db).filter(Employee_db.employee_id == employee_id).first()
+            if employee is None:
+                raise ValueError(f"An employee with ID {employee_id} does not exist!")
+            return {obj.name: getattr(employee, obj.name) for obj in employee.__table__.columns}
+        except Exception as e:
+            raise Exception(f"An error occurred getting the employee: {str(e)}")
+        finally:
+            # Close the session
+            self.session.close()
+
+    def all_employees(self):
+        try:
+            employees = self.session.query(Employee_db).all()
+            return [{obj.name: getattr(employee, obj.name) for obj in Customer_db.__table__.columns}
+                    for employee in employees]
+        except Exception as e:
+            raise Exception(f"An error occurred while getting all the employees: {str(e)}")
+        finally:
+            # Close the session
+            self.session.close()
+
+    def remove_employee(self, employee_id):
+        try:
+            # Find the employee with the given ID
+            r_employee = self.session.query(Employee_db).filter_by(employee_id=employee_id).first()
+
+            # If employee exists, remove it
+            self.session.delete(r_employee)
+            self.session.commit()
+            # Return it for confirmation
+            return {obj.name: getattr(r_employee, obj.name) for obj in r_employee.__table__.columns}
+        except Exception as e:
+            raise Exception(f"An error occurred while removing the customer: {str(e)}")
+        finally:
+            # Close the session
+            self.session.close()
+
