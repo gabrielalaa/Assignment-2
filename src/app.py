4dcd765b6ed05792bee1d6c78a4570105b475929
Rădulescu Carla Gabriela <carlagabrielaradulescu@gmail.com>
2024-06-20 18:41:00 +0200

    Include the route for employeeNS

--- src/app.py
+++ src/app.py
@@ -5,7 +5,7 @@ from flask_restx import Api
 # from .api.menuNS import menu_ns
 # from .api.orderNS import order_ns
 from .api.customerNS import customer_ns
-# from .api.employeeNS import employee_ns
+from .api.employeeNS import employee_ns
 
 from .model.agency import Agency
 
@@ -13,17 +13,17 @@ agency = Agency()
 
 
 def create_app():
-    paperroute_app = Flask(__name__)
+    mcroute_app = Flask(__name__)
     # need to extend this class for custom objects, so that they can be jsonified
-    paperroute_api = Api(paperroute_app, title="McDonalds: An app for both customers and employees")
+    mcroute_api = Api(mcroute_app, title="McDonalds: An app for both customers and employees")
 
     # add individual namespaces
-    # paperroute_api.add_namespace(menu_ns)
-    # paperroute_api.add_namespace(order_ns)
-    paperroute_api.add_namespace(customer_ns)
-    # paperroute_api.add_namespace(employee_ns)
+    # mcroute_api.add_namespace(menu_ns)
+    # mcroute_api.add_namespace(order_ns)
+    mcroute_api.add_namespace(customer_ns)
+    mcroute_api.add_namespace(employee_ns)
 
-    return paperroute_app
+    return mcroute_app
 
 
 if __name__ == '__main__':
